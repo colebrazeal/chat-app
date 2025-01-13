72853c4c29b208dc43593de4facd5cf48d72c450
colebrazeal <cbrazeal17@gmail.com>
2025-01-13 13:34:28 -0500

    updated json and yml

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+module.exports = {
+    output: 'export',  // This enables static export
+    reactStrictMode: true,  // Enable React strict mode for development
+    images: {
+      unoptimized: true,  // If using Next.js Image component and you want to handle images manually
+    },
+  };
+  
