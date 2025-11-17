828e2656dc1293e48734506a6115017aa13a7671
Cole Brazeal <cbrazeal17@gmail.com>
2025-11-17 15:52:49 -0500

    deployment test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-// next.config.js
-module.exports = {
-    output: 'export',  // This enables static export
-    reactStrictMode: true,  // Enable React strict mode for development
-    images: {
-      unoptimized: true,  // If using Next.js Image component and you want to handle images manually
-    },
-  };
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/chat-app',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
